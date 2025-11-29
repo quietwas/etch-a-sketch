6a17546e2a9fc3fe4758c36d18cf45ee138a833c
quietwas <r3dmike@gmail.com>
2025-11-29 12:18:50 -0500

    Add hover effect
    Created an event listener for each box which changes the background color once the pointer enters the box
    
    Changed the grid from creating just divs to divs with class box for more specificity and adjusted CSS to match

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
 const container = document.querySelector("#container")
 
+// Creates the 16x16 grid and applies class box to it
 for (i = 0; i < 272; i++) {
     const div = document.createElement("div")
+    div.className = "box"
     container.appendChild(div)
 }
+
+// Selects all boxes and then add an event listener for each box which changes the background color when the mouse touches it
+const boxes = document.querySelectorAll(".box")
+boxes.forEach((boxes) => {
+    boxes.addEventListener("mouseover", () => {
+        boxes.style.backgroundColor = "black"
+    })
+})
