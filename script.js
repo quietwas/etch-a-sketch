dfbbadb355467850d8046c0d0bc1979c2068b6cf
quietwas <r3dmike@gmail.com>
2025-11-29 21:02:38 -0500

    Change canvas and grid logic
    Before the canvas was made with same size pixels now it is made relative to the content-box size which allows for different sized pixels in the same size canvas
    
    Added two buttons to begin logic for changing the pixel size in the grid and randomize color as you hover over pixels

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
 const container = document.querySelector("#container")
 
-// Creates the 16x16 grid and applies class box to it
-for (i = 0; i < 272; i++) {
-    const div = document.createElement("div")
-    div.className = "box"
-    container.appendChild(div)
+// Creates a grid of AxA length when given a number less than 100
+function createGrid(n){
+    const size = 800
+    for (i = 0; i < n**2; i++) {
+        const div = document.createElement("div")
+        div.className = "box"
+        div.style.width = `${size / n}px`
+        div.style.height = `${size / n}px`
+        container.appendChild(div)
+    }
 }
+
+createGrid(16)
 
 // Selects all boxes and then add an event listener for each box which changes the background color when the mouse touches it
 const boxes = document.querySelectorAll(".box")
